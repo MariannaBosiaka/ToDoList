ad0a9a37bb026c28703e1821ccf252afff9536df
Marianna Bosiaka <57630657+MariannaBosiaka@users.noreply.github.com>
2024-06-24 12:04:04 +0300

    ability to check and uncheck boxes

--- todo.js
+++ todo.js
@@ -2,6 +2,7 @@ const addButton = document.querySelector(".button");
 const list = document.querySelector(".task-list");
 const input = document.querySelector(".add-task");
 
+
 addButton.addEventListener("click", function(){
     console.log('pressed the add button');
     let taskValue = input.value;
@@ -9,11 +10,33 @@ addButton.addEventListener("click", function(){
 });
 
 
-
 function addTask (taskName){
-    const task = document.createElement("li");
-    task.className = "task";
-    task.textContent = taskName;
-    list.appendChild(task);
+
+    if(taskName === '')
+    {
+        alert('must put a value in the box!');
+    }
+    else{
+        
+        const task = document.createElement("li");
+        task.className = "task";
+        task.textContent = taskName;
+        list.appendChild(task);
+    }
 }
 
+list.addEventListener("click", function(e){
+
+    
+    
+    if(e.target.tagName ==="LI")
+    {
+        console.log(e.target.classList.contains("LI"));
+        console.log('pressed the tasks');
+        e.target.classList.toggle("checked");
+    }
+
+    
+    
+    
+},false)
